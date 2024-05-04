5d3a58544b5dd45e8170c324d3a9ac3aa1d08276
colebrookson <cole.brookson@gmail.com>
2024-05-03 20:37:18 -0400

    moving over to ODEs

--- src/equations.jl
+++ src/equations.jl
@@ -9,3 +9,11 @@ z = x + y
 function dummy_project_function(x, y)
     return x + y
 end
+
+function lotka_volterra!(du, u, p, t)
+    N1, N2 = du
+    r1, r2, α11, α12, α21, α22 = p
+    du[1] = dN1 = N1 * (r1 - (α11 * N1) - (α12 * N2))
+    du[2] = dN2 = N2 * (r2 - (α11 * N1) - (α22 * N2))
+
+end
