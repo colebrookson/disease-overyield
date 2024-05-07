ed8bb3375435e77b6c4aad88df2f3522738bc70f
colebrookson <cole.brookson@gmail.com>
2024-05-07 13:44:17 -0400

    trying some stuff

--- scripts/development.jl
+++ scripts/development.jl
@@ -1,6 +1,20 @@
-using DrWatson
-@quickactivate "disease-overyield"
+# playing around with the odes 
 
-# Here you may include files from the source directory
-#include(srcdir("dummy_src_file.jl"))
+using DifferentialEquations, ModelingToolkit, OrdinaryDiffEq, Plots
+using ModelingToolkit: t_nounits as t, D_nounits as D
 
+function lotka_volterra!(du, u, p, t)
+    N₁, N₂ = u
+    r₁, r₂, α₁₁, α₁₂, α₂₁, α₂₂ = p
+    du[1] = dN₁ = N₁ * (r₁ - (α₁₁ * N₁) - (α₁₂ * N₂))
+    du[2] = dN₂ = N₂ * (r₂ - (α₂₁ * N₁) - (α₂₂ * N₂))
+end
+
+# compute the Jacobian matrix
+prob = ODEProblem(lotka_volterra!, [100, 100], (0, 1e5), (1, 1, 0.1, 0.1, 0.2, 0.2))
+# modeltoolkit transformation 
+@mtkbuild sys = modelingtoolkitize(prob)
+
+prob_jac = ODEProblem(sys, [], (0.0, 1e5), jac=true)
+lvjac = eval(generate_jacobian(sys))
+plot(sol)
--- src/equations.jl
+++ src/equations.jl
@@ -15,5 +15,5 @@ function lotka_volterra!(du, u, p, t)
     r1, r2, α11, α12, α21, α22 = p
     du[1] = dN1 = N1 * (r1 - (α11 * N1) - (α12 * N2))
     du[2] = dN2 = N2 * (r2 - (α11 * N1) - (α22 * N2))
-
 end
+
