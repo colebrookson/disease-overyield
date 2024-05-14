2ea8a5a1db1ca6e464d750519a565a542d212030
colebrookson <cole.brookson@gmail.com>
2024-05-14 14:26:23 -0400

    the equations for the latexify

--- scripts/development.jl
+++ scripts/development.jl
@@ -1,17 +1,67 @@
 # playing around with the odes 
 
-using DifferentialEquations, ModelingToolkit, OrdinaryDiffEq, Plots, Latexify,
-    ParameterizedFunctions
+using DifferentialEquations, ModelingToolkit, OrdinaryDiffEq, Plots, Latexify, ParameterizedFunctions
 using ModelingToolkit: t_nounits as t, D_nounits as D
 
 function lotka_volterra!(du, u, p, t)
     N₁, N₂ = u
-    r₁, r₂, α₁₂, α₂₁ = p
+    r₁, r₂, α₁₂, α₂₁, K₁, K₂ = p
     du[1] = dN₁ = r₁ * N₁ * (1 - ((N₁ + α₁₂ * N₂) / K₁))
     du[2] = dN₂ = r₂ * N₂ * (1 - ((N₂ + α₂₁ * N₁) / K₂))
 end
 
+function lotka_volterra_prop!(du, u, p, t)
+    N₁, N₂ = u
+    r₁, r₁ᵩ, r₂, α₁₂, α₂₁, K₁, K₂, ϕ = p
+    dN₁ = ((ϕ * r₁ᵩ * N₁) * (1 - ϕ) * r₁ * N₁) * (1 - ((N₁ + α₁₂ * N₂) / K₁))
+    dN₂ = r₂ * N₂ * (1 - ((N₂ + α₂₁ * N₁) / K₂))
+end
 # define also with parameterized functions
+lv = @ode_def basicLV begin
+    dN₁ = r₁ * N₁ * (1 - ((N₁ + α₁₂ * N₂) / K₁))
+    dN₂ = r₂ * N₂ * (1 - ((N₂ + α₂₁ * N₁) / K₂))
+end r₁ r₂ α₁₂ α₂₁ K₁ K₂
+latexify(lv)
+
+lv_prop = @ode_def lvProp begin
+    dN₁ = ((ϕ * r₁ᵩ * N₁) * (1 - ϕ) * r₁ * N₁) * (1 - ((N₁ + α₁₂ * N₂) / K₁))
+    dN₂ = r₂ * N₂ * (1 - ((N₂ + α₂₁ * N₁) / K₂))
+end r₁ r₁ᵩ r₂ α₁₂ α₂₁ K₁ K₂ ϕ
+latexify(lv_prop)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
